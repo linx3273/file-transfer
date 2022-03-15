49e548ec819d9d0c5fbca9616832bc16f15068ce
linx3273 <ayush96.gupta@gmail.com>
2022-03-15 19:39:43 +0530

    removed option to chose new connection/exit client mode as they were unecessary

--- src/client.py
+++ src/client.py
@@ -5,31 +5,17 @@ from tqdm import tqdm
 def client():
     SIZE = 1024
     port = 5050
-    while True:
-        funcs.inpmsg()
-        ip = input("Enter IP: ")    
+    
+    funcs.inpmsg()
+    ip = input("Enter IP: ")    
 
-        conn = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    conn = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-        conn.connect((ip,port))
-        data = conn.recv(SIZE).decode()
-        funcs.msg()
-        print(data)
+    conn.connect((ip,port))
+    data = conn.recv(SIZE).decode()
+    funcs.msg()
+    print(data)
 
-        if "Accepted" in data:
-            funcs.handleConnection(conn)
-        
-        print()
-        funcs.msg()
-        print("1. New Connection")
-        funcs.msg()
-        print("2. Close Client")
-
-        funcs.inpmsg()
-        c = int(input("Enter Choice: "))
-        if c==1:
-            continue
-        elif c==2:
-            return
-        else:
-            break
+    if "Accepted" in data:
+        funcs.handleConnection(conn)
+    return
